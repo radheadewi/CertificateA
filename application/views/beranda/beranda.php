e59742e2ab49f7b1063ee5b8eb5591c8517f9033
Radhea Permata Dewi <radhea.dewi@gmail.com>
2015-05-05 21:59:22 +0700

    Halaman tambah sertifikat ditambahkan

--- application/views/beranda/beranda.php
+++ application/views/beranda/beranda.php
@@ -8,9 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-
     <title>Beranda</title>
-
     <!-- Bootstrap Core CSS -->
     <link href="<?php echo base_url('asset/bower_components/bootstrap/dist/css/bootstrap.min.css');?>" rel="stylesheet">
 
@@ -28,13 +26,6 @@
 
     <!-- Custom Fonts -->
     <link href="<?php echo base_url('asset/bower_components/font-awesome/css/font-awesome.min.css');?>" rel="stylesheet">
-    
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
 
 </head>
 
@@ -54,7 +45,7 @@
                 <a class="navbar-brand" href="index.html">Certificate Authority KIJ</a>
             </div>
 
-    <ul class="nav navbar-top-links navbar-right">            
+            <ul class="nav navbar-top-links navbar-right">            
                 <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
                         <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
@@ -70,7 +61,6 @@
                     </ul>
                 </li>
             </ul>
- 
 
             <div class="navbar-default sidebar" role="navigation">
                 <div class="sidebar-nav navbar-collapse">
@@ -81,19 +71,14 @@
                         <br>
                         <br>
                         <li>
-                            <a href="index.html"><i class="fa fa-dashboard fa-fw"></i>Request Certificate</a>
+                            <a href="<?php echo site_url('add_certificate');?>"><i class="fa fa-dashboard fa-fw" ></i>Request Certificate</a>
                         </li>
-                        
                         <li>
                             <a href="tables.html"><i class="fa fa-table fa-fw"></i>Daftar Certificate</a>
                         </li>
-                        
-
                     </ul>
                 </div>
-                <!-- /.sidebar-collapse -->
             </div>
-            <!-- /.navbar-static-side -->
         </nav>
 
         <div id="page-wrapper">
@@ -103,28 +88,19 @@
                 </div>
             </div>
         </div>
-        <!-- /#page-wrapper -->
 
     </div>
-    <!-- /#wrapper -->
-
-    <!-- jQuery -->
     <script src="<?php echo base_url('asset/bower_components/jquery/dist/jquery.min.js');?>"></script>
-
     <!-- Bootstrap Core JavaScript -->
     <script src="<?php echo base_url('asset/bower_components/bootstrap/dist/js/bootstrap.min.js');?>"></script>
-
     <!-- Metis Menu Plugin JavaScript -->
     <script src="<?php echo base_url('asset/bower_components/metisMenu/dist/metisMenu.min.js');?>"></script>
-
     <!-- Morris Charts JavaScript -->
     <script src="<?php echo base_url('asset/bower_components/raphael/raphael-min.js');?>"></script>
     <script src="<?php echo base_url('asset/bower_components/morrisjs/morris.min.js');?>"></script>
     <script src="<?php echo base_url('asset/js/morris-data.js');?>"></script>
-
     <!-- Custom Theme JavaScript -->
     <script src="<?php echo base_url('asset/dist/js/sb-admin-2.js');?>"></script>
-
 </body>
 
 </html>
